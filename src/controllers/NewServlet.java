4fe20a6f7c2a77d8cd1bf7a625b2dbef7a696158
masaki.ikebe <masaki,ikebe@riskmonster.co.jp>
2021-11-29 15:00:23 +0900

    NewServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -9,22 +9,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import models.Message;
-
-
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,13 +32,31 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em =DBUtil.createEntityManager();
+        em.getTransaction().begin();
+        Message m= new Message();
 
-        EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
 
+        String title ="taro";
+        m.setTitle(title);
+
+        String content ="hello";
+        m.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        em.persist(m);
+        em.getTransaction().commit();
+
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
         em.close();
+
+
+
+
 
     }
 
